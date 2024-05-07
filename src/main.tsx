e280e495cb088d3d0cef91c9a78556801992e5b1
Thiago Alves <80608138+Thiagof2755@users.noreply.github.com>
2024-05-07 11:46:32 -0300

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Import BrowserRouter, Routes, and Route
 import './index.scss'
+import App from "./App";
 
-import Home from './Pages/Home'
+import Home from "./Pages/Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
           <Route path="/" element={<Home />} />
-
         </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
+
+
