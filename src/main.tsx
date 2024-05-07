5ff0fe9c17e80b468cd062095b572a140ea8b9e9
Thiago Alves <80608138+Thiagof2755@users.noreply.github.com>
2024-05-07 11:37:26 -0300

    teste build

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,21 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import App from './App.tsx'
 import './index.scss'
 
 import Home from './Pages/Home'
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<App />}>
-            <Route path="/" element={<Home />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </React.StrictMode>,
-  );
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
