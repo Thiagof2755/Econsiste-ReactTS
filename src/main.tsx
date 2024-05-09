d164869e2144b9f8d384bb6b71e7c2eb623cd438
Thiago Alves <80608138+Thiagof2755@users.noreply.github.com>
2024-05-09 16:33:55 -0300

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom"; // Import BrowserRouter, Routes, and Route
 import './index.scss'
 import App from "./App.tsx";
+import Cookies from 'js-cookie';
 
 import Home from "./Pages/Home";
 
+// Define um cookie com SameSite=None; Secure
+Cookies.set('Cookie', 'Cookie', { sameSite: 'None', secure: true });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
